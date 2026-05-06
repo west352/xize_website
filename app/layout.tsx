807b6178853f47c9ad37c9c38da3a586172a8e9f
Changjiang Liu <changjiangliu@Changjiangs-MBP.home.local>
2026-05-05 22:02:18 -0700

    change email

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               contactPoint: {
                 '@type': 'ContactPoint',
                 contactType: 'sales',
-                email: 'xize.autoparts@gmail.com',
+                email: 'sales@xizeautoparts.com',
               },
             }),
           }}
